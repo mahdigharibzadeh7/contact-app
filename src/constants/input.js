e4154e5d6f96b6dbf340ee11800835c8012cd48a
mahdigharibzadeh7 <gharibzadehmahdi7@gmail.com>
2024-07-09 10:31:49 +0330

    seperate inputs in aonther js file

---
+++ src/constants/input.js
@@ -0,0 +1,36 @@
+const inputs = [
+  {
+    id: 1,
+    type: "text",
+    name: "name",
+    placeholder: "Name",
+    className:
+      "col-span-6 border-2 my-2 w-[28rem] px-4 py-1 rounded-md focus:outline-blue-700 text-blue-7",
+  },
+  {
+    id: 2,
+    type: "text",
+    name: "lastName",
+    placeholder: "Last Name",
+    className:
+      "col-span-6 border-2 my-2 w-[28rem] px-4 py-1 rounded-md focus:outline-blue-700 text-blue-7",
+  },
+  {
+    id: 3,
+    type: "email",
+    name: "email",
+    placeholder: "Email",
+    className:
+      "col-span-6 border-2 my-2 w-[28rem] px-4 py-1 rounded-md focus:outline-blue-700 text-blue-7",
+  },
+  {
+    id: 4,
+    type: "text",
+    name: "phone",
+    placeholder: "Phone",
+    className:
+      "col-span-6 border-2 my-2 w-[28rem] px-4 py-1 rounded-md focus:outline-blue-700 text-blue-7",
+  },
+];
+
+export default inputs;
